b233e36bfd374863348b2224de5234bd95b429fc
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-14 10:30:45 +0200

    Create models with migration

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True)
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(to=Tag, related_name="tags")
+
+    def __str__(self):
+        return self.content
