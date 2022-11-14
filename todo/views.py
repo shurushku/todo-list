618810cbc78f6b87cc9c95a1d9d34fdd082a8740
shurushku <nastia.kuchina.741456963@gmail.com>
2022-11-14 11:26:38 +0200

    Create task and tag pages

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,59 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from .forms import TaskForm, TagForm
+from .models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+
+
+def toggle_task(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.done:
+        task.done = False
+    else:
+        task.done = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("todo:task-list"))
